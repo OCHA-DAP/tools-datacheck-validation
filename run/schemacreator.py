561d532252b400693beb7ce2bfa56943ccddd3f0
Mike <rans@email.com>
2018-04-30 14:18:46 +0200

    Use new utils in HDX Utils

--- run/schemacreator.py
+++ run/schemacreator.py
@@ -5,6 +5,8 @@ from os.path import join
 
 from bs4 import BeautifulSoup
 from hdx.utilities.downloader import Download
+from hdx.utilities.loader import load_json
+from hdx.utilities.saver import save_json
 
 start_url = 'http://gistmaps.itos.uga.edu/arcgis/rest/services/COD_External'
 base_url = 'http://gistmaps.itos.uga.edu/arcgis/rest/services/COD_External/%s_pcode/FeatureServer'
@@ -31,31 +33,30 @@ with Download() as downloader:
         ind = elementstr.find('pcode')
         iso3 = elementstr[ind-4:ind-1]
         countryisos.add(iso3)
-    with open('validation-schema-pcodes.json', 'rt') as f:
-        template = json.loads(f.read(), object_pairs_hook=OrderedDict)
-        for iso3 in sorted(countryisos):
-            print(iso3)
-            url = base_url % iso3
-            response = downloader.download(url)
-            soup = BeautifulSoup(response.text, 'html5lib')
-            adminlevels = list()
-            for element in soup.findAll(text=re.compile('Admin[1-5]')):
-                adminlevels.append(str(element)[-1])
-            print(adminlevels)
-            schema = list()
-            admrules = list()
-            for rule_template in template:
-                if '{ADM}' in str(rule_template):
-                    admrules.append(rule_template)
-                else:
-                    for adminlevel in adminlevels:
-                        for admrule in admrules:
-                            schema.append(get_rule(admrule, iso3, adminlevel))
-                    admrules = list()
-                    schema.append(get_rule(rule_template, iso3))
-            for adminlevel in adminlevels:
-                for admrule in admrules:
-                    schema.append(get_rule(admrule, iso3, adminlevel))
-            with open(join('pcodes', 'validation-schema-pcodes-%s.json' % iso3.lower()), 'w') as outfile:
-                json.dump(schema, outfile, indent=2)
+
+    template = load_json('validation-schema-pcodes.json')
+    for iso3 in sorted(countryisos):
+        print(iso3)
+        url = base_url % iso3
+        response = downloader.download(url)
+        soup = BeautifulSoup(response.text, 'html5lib')
+        adminlevels = list()
+        for element in soup.findAll(text=re.compile('Admin[1-5]')):
+            adminlevels.append(str(element)[-1])
+        print(adminlevels)
+        schema = list()
+        admrules = list()
+        for rule_template in template:
+            if '{ADM}' in str(rule_template):
+                admrules.append(rule_template)
+            else:
+                for adminlevel in adminlevels:
+                    for admrule in admrules:
+                        schema.append(get_rule(admrule, iso3, adminlevel))
+                admrules = list()
+                schema.append(get_rule(rule_template, iso3))
+        for adminlevel in adminlevels:
+            for admrule in admrules:
+                schema.append(get_rule(admrule, iso3, adminlevel))
+        save_json(schema, join('pcodes', 'validation-schema-pcodes-%s.json' % iso3.lower()), pretty=True)
 
