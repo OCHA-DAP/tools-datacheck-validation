18f9025c786c96fcc8cc6ac99d5c7eaa1997435b
Mike <rans@email.com>
2018-05-02 14:21:36 +0200

    Fix schemacreator.py

--- run/schemacreator.py
+++ run/schemacreator.py
@@ -57,35 +57,6 @@ def schemacreator(start_url, base_url, template_path, output_folder):
                     schema.append(get_rule(admrule, iso3, adminlevel))
             save_json(schema, join(output_folder, 'validation-schema-pcodes-%s.json' % iso3.lower()), pretty=True)
 
-    template = load_json(join('..', 'validation-schema-pcodes.json'))
-    for iso3 in sorted(countryisos):
-        print(iso3)
-        url = base_url % iso3
-        response = downloader.download(url)
-        adminlevels = set()
-        for layer in response.json()['layers']:
-            layername = layer['name'].lower()
-            if 'feature' in layer['type'].lower() and 'admin' in layername and not 'lines' in layername:
-                adminlevel = int(layername[-1])
-                if adminlevel > 0:
-                    adminlevels.add(adminlevel)
-        adminlevels = sorted(list(adminlevels))
-        print(adminlevels)
-        schema = list()
-        admrules = list()
-        for rule_template in template:
-            if '{ADM}' in str(rule_template):
-                admrules.append(rule_template)
-            else:
-                for adminlevel in adminlevels:
-                    for admrule in admrules:
-                        schema.append(get_rule(admrule, iso3, adminlevel))
-                admrules = list()
-                schema.append(get_rule(rule_template, iso3))
-        for adminlevel in adminlevels:
-            for admrule in admrules:
-                schema.append(get_rule(admrule, iso3, adminlevel))
-        save_json(schema, join('..', 'pcodes', 'validation-schema-pcodes-%s.json' % iso3.lower()), pretty=True)
 
 if __name__ == '__main__':
     start_url = 'http://gistmaps.itos.uga.edu/arcgis/rest/services/COD_External?f=pjson'
